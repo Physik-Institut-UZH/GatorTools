84148fb69196bfd5b1b35eb33dca97274a747462
Francesco Piastra <f.piastra@gmail.com>
2026-02-18 18:56:34 +0100

    Better variables usage in the watchdog script

--- scripts/EnsureTrigMonitoring.sh
+++ scripts/EnsureTrigMonitoring.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
+source $HOME/.bashrc
 source $HOME/.localenv # Here is where the $LOCALSYS and all the other paths are defined
 
-PROC_NAME="process_gator_trigrate.py"
 SESSION_NAME="GatorTrigRate"
 
 LOG_DIR="$LOCALSYS/var/log/gator_trigmon"
 CRON_LOG="$LOCALSYS/var/log/cron/EnsureTrigMonitoring.log"
+CRON_ERR="$LOCALSYS/var/log/cron/EnsureTrigMonitoring.err"
 
 mkdir -p "$LOG_DIR"
 mkdir -p "$(dirname "$CRON_LOG")"
 
 
 # Check if process is running
-if pgrep -f "$PROC_NAME" > /dev/null ; then
+if pgrep -f "[p]rocess_gator_trigrate.py" > /dev/null ; then
   echo "$(date) : Script running" >> "$CRON_LOG"
     exit 0
 else 
   echo "$(date) : Restarting Script" >> "$CRON_LOG"
 
-  screen -d -m -S "$SESSION_NAME" bash -c "
+  screen -d -m -S "$SESSION_NAME" bash -c '
         source ~/.bashrc
         source ~/.localenv
         
         conda activate gator
 
-        exec python $PROC_NAME 2>> $LOG_DIR/trigmon.err 2>&1
-    "
+        exec process_gator_trigrate.py 2>> '"$CRON_ERR"'
+    '
 
   exit 0
 fi
