c62a661b62ec4288b734b55290f3e25e9a3f761f
Francesco Piastra <f.piastra@gmail.com>
2026-02-26 14:53:54 +0100

    Minor fix to the trigger monitoring auomatic restart

--- scripts/EnsureTrigMonitoring.sh
+++ scripts/EnsureTrigMonitoring.sh
@@ -3,11 +3,13 @@
 source $HOME/.bashrc
 source $HOME/.localenv # Here is where the $LOCALSYS and all the other paths are defined
 
-SESSION_NAME="GatorTrigRate"
+SESSION_NAME="GatorTrigRateNew"
 
 LOG_DIR="$LOCALSYS/var/log/gator_trigmon"
 CRON_LOG="$LOCALSYS/var/log/cron/EnsureTrigMonitoring.log"
 CRON_ERR="$LOCALSYS/var/log/cron/EnsureTrigMonitoring.err"
+
+GATOR_DAQPROC_FILE="$LOCALSYS/etc/gator_daq_proc/GatorDaqProc.json"
 
 mkdir -p "$LOG_DIR"
 mkdir -p "$(dirname "$CRON_LOG")"
@@ -17,17 +19,17 @@ mkdir -p "$(dirname "$CRON_LOG")"
 if pgrep -f "[p]rocess_gator_trigrate.py" > /dev/null ; then
   echo "$(date) : Script running" >> "$CRON_LOG"
     exit 0
-else 
+else
   echo "$(date) : Restarting Script" >> "$CRON_LOG"
 
-  screen -d -m -S "$SESSION_NAME" bash -c '
+  screen -d -m -S "$SESSION_NAME" bash -c "
         source ~/.bashrc
         source ~/.localenv
-        
+
         conda activate gator
 
-        exec process_gator_trigrate.py 2>> '"$CRON_ERR"'
-    '
+        exec process_gator_trigrate.py $GATOR_DAQPROC_FILE 2>> $CRON_ERR
+    "
 
   exit 0
 fi
