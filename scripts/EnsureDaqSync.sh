cda6fde4705fa9356e9e19dd33d19707ac46ab8d
Francesco Piastra <f.piastra@gmail.com>
2026-02-27 12:22:36 +0100

    Removed the redirection of the errors to the supervisor error files as it does not work and also the stdout get redirected there.

--- scripts/EnsureDaqSync.sh
+++ scripts/EnsureDaqSync.sh
@@ -5,30 +5,29 @@ source $HOME/.localenv # Here is where the $LOCALSYS and all the other paths are
 
 SESSION_NAME="GatorDaqSync"
 
-LOG_DIR="$LOCALSYS/var/log/gator_daq_sync"
+CONDA_ENV="gator"
+PYTHON_EXEC="$HOME/miniconda3/envs/$CONDA_ENV/bin/python"
+
 CRON_LOG="$LOCALSYS/var/log/cron/EnsureDaqSync.log"
 CRON_ERR="$LOCALSYS/var/log/cron/EnsureDaqSync.err"
 
+GATOR_DAQ_SYNC_SCRIPT="gator_daq_sync.py"
+SCRIPT_PATH="$LOCALSYS/bin/$GATOR_DAQ_SYNC_SCRIPT"
 GATOR_DAQSYNC_JSON="$LOCALSYS/etc/GatorDaqSync/config.json"
 
-mkdir -p "$LOG_DIR"
-mkdir -p "$(dirname "$CRON_LOG")"
-
-
 # Check if process is running
-if pgrep -f "[g]ator_daq_sync.py" > /dev/null ; then
+if pgrep -f "$PYTHON_EXEC $SCRIPT_PATH" > /dev/null ; then
   echo "$(date) : Script running" >> "$CRON_LOG"
     exit 0
 else
   echo "$(date) : Restarting Script" >> "$CRON_LOG"
+  echo -e "\n\n$(date) : Restarting Screen session" >> "$CRON_ERR"
 
   screen -d -m -S "$SESSION_NAME" bash -c "
-        source ~/.bashrc
-        source ~/.localenv
-
-        conda activate gator
-
-        exec gator_daq_sync.py $GATOR_DAQSYNC_JSON 2>> $CRON_ERR
+        source $HOME/.bashrc;
+        source $HOME/.localenv;
+	PTY=\$(tty)
+	exec $PYTHON_EXEC $SCRIPT_PATH $GATOR_DAQSYNC_JSON;
     "
 
   exit 0
